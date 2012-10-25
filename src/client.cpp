8a48566f5431d62db16b81a07a8bde52d1197c82
Christopher Cole <cole14@purdue.edu>
2012-10-25 15:21:47 -0400

    Improve the way hostname/port are read from the user during bootstraping.

--- src/client.cpp
+++ src/client.cpp
@@ -36,20 +36,61 @@ void client::receive(unsigned char *buf, size_t buf_len){
     fprintf(stdout, "\n");
 }
 
+static std::string read_hostname(void){
+    size_t n = BUFSIZ;
+    char lne[n];
+    ssize_t len = 0;
+
+    //Read the line
+    if(NULL == fgets(lne, n, stdin)){
+        error(-1, errno, "Unable to read hostname");
+    }
+
+    //Get rid of the newline terminator
+    len = strlen(lne) - 1;
+    while(lne[len] == '\r' || lne[len] == '\n')
+        lne[len--] = '\0';
+
+    return std::string(lne);
+}
+
+static int read_port(void){
+    long port = 0;
+    size_t n = 128;
+    char lne[n];
+
+    //Read the line
+    if(NULL == fgets(lne, n, stdin)){
+        error(-1, errno, "Unable to read hostname");
+    }
+
+    //parse the port
+    port = strtol(lne, NULL, 10);
+    if(port <= INT_MIN || port >= INT_MAX){
+        error(-1, ERANGE, "Unable to read port");
+    }
+    if(port == 0){
+        error(-1, EINVAL, "Port must be in range [1, 65535]");
+    }
+
+    return port;
+}
+
 void client::run_cli(){
     int port = 0;
-    std::string bootstrap_host;
 
     //Get the bootstrap node's hostname:port from the user
     fprintf(stdout, "Enter bootstrap hostname: ");
-    std::cin >> bootstrap_host;
+    std::string bootstrap_host(read_hostname());
+
     fprintf(stdout, "Enter bootstrap port: ");
-    std::cin >> port;
+    port = read_port();
 
     //Connect to the broadcast group through the bootstrap node
     if(!chan->connect(bootstrap_host, port)){
         error(-1, 0, "Unable to connect to bootstrap node!");
     }
+
     fprintf(stdout, "Successfully connected!\n");
 }
 
