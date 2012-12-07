92111438f8b9effefb9bc9aa9ef2ce874abbfc00
Christopher Cole <cole14@purdue.edu>
2012-12-07 12:34:43 -0500

    Allow the compiler to type-check the logger format specifiers for printf-compliance.

--- src/logger.h
+++ src/logger.h
@@ -8,7 +8,7 @@ class logger {
         logger(int level, FILE *fp);
         logger(int level, const char *f_name);
         ~logger();
-        void log(int level, const char *str, ...);
+        void log(int level, const char *str, ...) __attribute__((format(printf, 3, 4)));
         int get_level();
         void set_level(int new_level);
     private:
