2b58b9f7d80dadabf51ec7ed49358dd643b5fdda
Dan Roberts <droberts@purdue.edu>
2013-01-10 12:12:51 -0500

    Forgot to save.

--- src/enc/cooperative_encoder.h
+++ src/enc/cooperative_encoder.h
@@ -17,17 +17,7 @@ class Cooperative_Encoder : public Encoder {
         size_t get_chunk(unsigned char **dest, unsigned chunk_id);
 
     private:
-        unsigned char *data;
-        size_t data_pos;
-        size_t data_len;
-        size_t chunk_len;
-        size_t num_peers;
-        size_t total_chunks;
         size_t chunks_per_peer;
-        size_t current_peer_chunks;
-
-        bool descriptor_sent;
-        unsigned int chunk_id;
 };
 
 #endif /* __COOPERATIVE_ENCODER_H */
